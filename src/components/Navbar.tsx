3655a09c12e3110f8c0fe9516a7127d68293f950
Dogukan Caltekin <dcaltekin@gmail.com>
2022-09-20 07:40:20 +0300

    signout-added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 function Navbar() {
   console.log(auth.currentUser?.displayName);
   const [user] = useAuthState(auth);
+  const logOut = async () => {
+    await signOut(auth);
+  };
   return (
     <div>
       <Link to="/"> Home</Link>
@@ -13,6 +17,7 @@ function Navbar() {
       <div>
         <p>{user?.displayName}</p>
         <img src={user?.photoURL || ""} />
+        <button onClick={logOut}>Logout</button>
       </div>
     </div>
   );
